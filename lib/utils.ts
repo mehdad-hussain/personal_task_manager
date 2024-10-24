a8146331f3746e4939d3836792968790811d3933
mehdad-hussain <hussain.mehdad1@gmail.com>
2024-10-24 18:07:53 +0600

    feat: add few custom hooks and utils function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,54 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Priority, Status } from "@/features/tasks/model/tasks";
+import {
+  CheckCircledIcon,
+  QuestionMarkCircledIcon,
+  StopwatchIcon,
+} from "@radix-ui/react-icons";
+import { clsx, type ClassValue } from "clsx";
+import {
+  ArrowDownIcon,
+  ArrowRightIcon,
+  ArrowUpIcon,
+  CircleIcon,
+} from "lucide-react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getStatusIcon(status: Status) {
+  const statusIcons = {
+    [Status.PENDING]: QuestionMarkCircledIcon,
+    [Status.IN_PROGRESS]: StopwatchIcon,
+    [Status.COMPLETED]: CheckCircledIcon,
+  };
+
+  return statusIcons[status] || CircleIcon;
+}
+
+export function getPriorityIcon(priority: Priority) {
+  const priorityIcons = {
+    [Priority.LOW]: ArrowDownIcon,
+    [Priority.MEDIUM]: ArrowRightIcon,
+    [Priority.HIGH]: ArrowUpIcon,
+  };
+
+  return priorityIcons[priority] || CircleIcon;
+}
+
+export function formatDate(
+  date: Date | string | number,
+  opts: Intl.DateTimeFormatOptions = {},
+) {
+  return new Intl.DateTimeFormat("en-US", {
+    month: opts.month ?? "long",
+    day: opts.day ?? "numeric",
+    year: opts.year ?? "numeric",
+    ...opts,
+  }).format(new Date(date));
+}
+
+export function capitalize(text: string): string {
+  return text.charAt(0).toUpperCase() + text.slice(1);
 }
